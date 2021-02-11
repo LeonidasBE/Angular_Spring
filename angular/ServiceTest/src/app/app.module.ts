ae0ac9f0af6c3767f3577aab8a1087ffd152b772
LeonidasBE <leonidasleo10@gmail.com>
2021-02-10 22:40:45 -0300

    First-angular-cli commit

---
+++ angular/ServiceTest/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { HttpClientModule } from '@angular/common/http';
+import { CommentService } from './comments/comment.service';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+ 
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CommentsComponent } from './comments/comments.component';
+ 
+@NgModule({
+  declarations: [
+    AppComponent,
+    CommentsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [CommentService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ angular/ServiceTest/src/app/comments/comment.modelo.ts
@@ -0,0 +1,5 @@
+export class Comment {
+    private id: number
+    name: string
+    email: string
+}
