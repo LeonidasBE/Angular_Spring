a16f8efcc2b4aa27b1b1c4107bb4d5fa6d40c362
Matheus-Goebel <Matheus_lol_@hotmail.com>
2025-03-04 20:54:18 -0300

    teste

--- angular/helpdesk/src/app/app.component.ts
+++ angular/helpdesk/src/app/app.component.ts
@@ -4,7 +4,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { MenuComponent } from './components/menu/menu.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { FormsModule } from '@angular/forms';
-
+// banana
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, HeaderComponent, MenuComponent, FooterComponent, FormsModule],
