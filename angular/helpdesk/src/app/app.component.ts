56911296d317576430a39392ab260b9745b11889
Matheus-Goebel <Matheus_lol_@hotmail.com>
2025-03-04 20:58:11 -0300

    Import deleted

--- angular/helpdesk/src/app/app.component.ts
+++ angular/helpdesk/src/app/app.component.ts
@@ -3,11 +3,10 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { MenuComponent } from './components/menu/menu.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { FormsModule } from '@angular/forms';
-// banana
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, MenuComponent, FooterComponent, FormsModule],
+  imports: [RouterOutlet, HeaderComponent, MenuComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
